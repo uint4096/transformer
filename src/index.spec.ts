23ae04103db36922118480b2c2397b866b91da07
uint4096 <abhishek.kumar251095@gmail.com>
2023-08-03 12:33:49 +0530

    test: plain to instance

---
+++ src/index.spec.ts
@@ -0,0 +1,33 @@
+import { expose, plainToInstance, transform } from "./index";
+
+describe("Transformer", () => {
+  it("should transform an object to an instance", () => {
+    class User {
+      @transform((user) => parseInt(user.user_id, 10))
+      id: number;
+
+      @transform((user) => `${user.first_name} ${user.last_name}`)
+      name: string;
+
+      @expose()
+      email: string;
+    }
+
+    const sourceUser = {
+      email: "name@email.com",
+      first_name: "Name",
+      last_name: "Surname",
+      user_id: "122343",
+    };
+
+    const transformedUser: User = plainToInstance(sourceUser, User);
+    expect(transformedUser).toBeDefined();
+    expect(transformedUser).toEqual(
+      expect.objectContaining({
+        id: 122343,
+        name: "Name Surname",
+        email: "name@email.com",
+      })
+    );
+  });
+});
